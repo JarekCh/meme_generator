9fb8af4d155308cde748124f601a0ddbe5c8331f
Jaroslaw Charchula <charchula@gmail.com>
2022-06-17 20:48:47 +0200

    add object file. add useState with conditional rendering in Meme component.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react';
+import memesData from '../memesData.js';
 
 
 function Meme() {
-  return (    
-    <main>
-        <form className="form">
-            <input 
-                type="text"
-                placeholder="Top text"
-                className="form--input"
-            />
-            <input 
-                type="text"
-                placeholder="Bottom text"
-                className="form--input"
-            />
-            <button 
-                className="form--button"
-            >
-                Get a new meme image 🖼
-            </button>
-        </form>
-    </main>
-  )
+    const [memeImage, setMemeImage] = useState("");
+
+    const getMeme = () => {
+        const getRandomImg = Math.floor(Math.random() * memesData.data.memes.length);        
+        const imgRandomUrl = memesData.data.memes[getRandomImg].url;
+        setMemeImage(imgRandomUrl);     
+    }
+    
+
+    return (    
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMeme}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>            
+            {memeImage && <img src={memeImage} alt="meme image" className="meme--image" />}   
+        </main>
+    )
 }
 
 export default Meme
